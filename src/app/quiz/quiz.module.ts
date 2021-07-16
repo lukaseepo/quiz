a7893c577a15664807e3a0ea2fc9589b08ec7af4
luka sefashvili <luksepashvili@gmail.com>
2021-07-16 16:32:28 +0400

    feat: add quiz module

---
+++ src/app/quiz/quiz.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { QuizRoutingModule } from './quiz.routing.module';
+
+
+
+@NgModule({
+  declarations: [
+   
+  ],
+  imports: [
+    BrowserModule,
+    QuizRoutingModule
+  ],
+})
+export class QuizModule { }
+
